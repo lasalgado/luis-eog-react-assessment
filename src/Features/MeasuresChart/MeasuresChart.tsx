be91642db7439ba075dfcfb1c3e08b661c39baa8
Luis Arturo Salgado Orta <laso_360@yahoo.com.mx>
2021-09-21 12:52:15 -0500

    feat: add yxis lines depending on the units available

--- src/Features/MeasuresChart/MeasuresChart.tsx
+++ src/Features/MeasuresChart/MeasuresChart.tsx
@@ -1,8 +1,4 @@
-import React, {
-  FC,
-  useEffect,
-  useState,
-} from 'react';
+import React, { FC } from 'react';
 import { Paper } from '@material-ui/core';
 import {
   LineChart,
@@ -14,8 +10,7 @@ import {
   Legend,
 } from 'recharts';
 import { useStyles } from './MeasuresChart.styles';
-import { getSelected } from '../../redux/metrics/selector';
-import { getMeasurements } from '../../redux/measurements/selector';
+import { getChartData } from '../../redux/measurements/selector';
 import { useAppSelector } from '../../redux/hooks';
 import { IChartData } from '../../types/interfaces';
 import {
@@ -37,74 +32,98 @@ const customizedAxisTick = (props: { x: any; y: any; payload: any }) => {
   );
 };
 
-const renderLineChart = (series: IChartData[]) => (
-  <LineChart
-    width={400}
-    height={250}
-    margin={{
-      top: 20,
-      right: 30,
-      left: 20,
-      bottom: 0,
-    }}
-    style={{ backgroundColor: 'white' }}
-  >
-    <XAxis
-      dataKey='at'
-      interval='preserveEnd'
-      scale='time'
-      type="number"
-      domain={['dataMin', 'dataMax']}
-      tick={customizedAxisTick}
-    />
-    <YAxis dataKey='value' label={{ value: '°F', angle: -90, position: 'insideLeft' }} />
-    <Tooltip
-      labelFormatter={
-        (timestamp: number): string => timeToStringSingle(timestamp)
-      }
-      isAnimationActive={false}
-      formatter={(value: any, name: any, props: any) => `${value} ${formatUnits(props.payload.unit)}`}
-    />
-    <Legend />
-    {series.map((s, index) => (
-      <Line
-        dataKey="value"
-        data={s.data}
-        name={s.name}
-        key={s.name}
-        stroke={getColor(index)}
-        type='monotone'
-        dot={false}
-        activeDot={false}
-        isAnimationActive={false}
+const customizedYAxisTick = (props: { x: any; y: any; payload: any }) => {
+  const { x, y, payload } = props;
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text x={-10} y={0} dy={5} textAnchor="middle" fontSize={10} fill="#757575">
+        {payload.value}
+      </text>
+    </g>
+  );
+};
+
+const renderLineChart = (chartData: { series: IChartData[], units: string[] }) => {
+  const { series, units } = chartData;
+
+  return (
+    <LineChart
+      width={400}
+      height={250}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 0,
+      }}
+      style={{ backgroundColor: 'white' }}
+    >
+      <XAxis
+        dataKey='at'
+        interval='preserveEnd'
+        scale='time'
+        type="number"
+        domain={['dataMin', 'dataMax']}
+        tick={customizedAxisTick}
       />
-    ))}
-  </LineChart>
-);
+      {units.map((s) => (
+        <YAxis
+          yAxisId={`yAxis-${s}`}
+          dataKey='value'
+          key={`yAxis-${s}`}
+          type='number'
+          tick={customizedYAxisTick}
+          unit={s}
+          label={{
+            value: formatUnits(s),
+            angle: -90,
+            position: 'insideTopLeft',
+          }}
+        />
+      ))}
+      <Tooltip
+        labelFormatter={
+          (timestamp: number): string => timeToStringSingle(timestamp)
+        }
+        isAnimationActive={false}
+        formatter={(value: any, name: any, props: any) => `${value} ${formatUnits(props.payload.unit)}`}
+      />
+      <Legend verticalAlign="top" height={60} />
+      {series.map((s, index) => {
+        const unit = s.data.length > 0 ? s.data[0].unit : '';
+
+        return (
+          <Line
+            dataKey="value"
+            data={s.data}
+            name={s.name}
+            key={s.name}
+            yAxisId={`yAxis-${unit}`}
+            stroke={getColor(index)}
+            type='monotone'
+            dot={false}
+            activeDot={false}
+            isAnimationActive={false}
+          />
+        );
+      })}
+    </LineChart>
+  );
+};
 
 const MeasuresChart: FC<{
   beat: number
 }> = ({ beat }) => {
   const classes = useStyles();
-  const [data, setData] = useState<IChartData[]>([]);
-  const measurements = useAppSelector(getMeasurements);
-  const selectedMetrics = useAppSelector(getSelected);
+  const chartData = useAppSelector(getChartData);
 
-  useEffect(() => {
-    const entries = Object.entries(measurements);
-    const filteredEntries = entries.filter(([key]) => selectedMetrics.indexOf(key) !== -1);
-    const series = filteredEntries.map(([, value]) => value);
-
-    setData(series);
-  }, [measurements, selectedMetrics]);
-
-  if (beat < -1 || data.length < 1) return null;
+  if (beat < -1 || chartData.series.length < 1) return null;
 
   return (
     <Paper className={classes.paper}>
       <div style={{ width: '100%', height: 400 }}>
         <ResponsiveContainer>
-          {renderLineChart(data)}
+          {renderLineChart(chartData)}
         </ResponsiveContainer>
       </div>
     </Paper>
--- src/redux/measurements/selector.ts
+++ src/redux/measurements/selector.ts
@@ -1,9 +1,24 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../store';
+import { getSelected } from '../metrics/selector';
 
 export const getMeasurements = (state: RootState) => state.measurements;
 
 export const getExistingMetrics = createSelector(
   getMeasurements,
   (measurements) => Object.keys(measurements),
+);
+
+export const getChartData = createSelector(
+  getMeasurements,
+  getSelected,
+  (measurements, selected) => {
+    const entries = Object.entries(measurements);
+    const filteredEntries = entries.filter(([key]) => selected.indexOf(key) !== -1);
+    const series = filteredEntries.map(([, value]) => value);
+    const uniqueSet = new Set(series.map((entry) => (entry.data.length > 0 ? entry.data[0].unit : '')));
+    const units = Array.from(uniqueSet);
+
+    return { series, units };
+  },
 );
